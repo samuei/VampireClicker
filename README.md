564407c0d66530a0c86dcd5bb840c5287fdbe3a5
Samuel A. Lamb <samuei@users.noreply.github.com>
2017-09-20 00:11:45 -0500

    Initial Commit
    Right now, you are a perpetual revenant, only able to hunt once per night and learning nothing from your experiences.
    
    One can only hope that future generations will do better.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # VampireClicker
+
+You are a blood-sucking creature of the night. 
+
+You require the blood of the living to animate your soulless corpse every sunset. Perhaps as you grow and learn about the
+night, you will become able to do more than simply feed and sleep. Perhaps. Or perhaps not.
---
+++ vampscript.js
@@ -0,0 +1,53 @@
+
+var change_blood = (function () {
+    var counter = 1;
+    return function (op, amt) {
+		if (op == '+') return counter += amt;
+		else if (op == '-') return counter -= amt;
+	}
+})();
+
+var change_energy = (function () {
+    var counter = 1;
+    return function (op, amt) {
+		if (op == '+') return counter += amt;
+		else if (op == '-') return counter -= amt;
+	}
+})();
+
+var change_experience = (function () {
+    var counter = 0;
+    return function (op, amt) {
+		if (op == '+') return counter += amt;
+		else if (op == '-') return counter -= amt;
+	}
+})();
+
+var change_night = (function () {
+    var counter = 0;
+    return function (op, amt) {
+		if (op == '+') return counter += amt;
+		else if (op == '-') return counter -= amt;
+	}
+})();
+
+function drink(){
+	var bld = change_blood('+', 1),
+		en = change_energy('-', 1),
+		exp = change_experience('+', 1);
+    document.getElementById("blood").innerHTML = bld;
+    document.getElementById("energy").innerHTML = en;
+    document.getElementById("experience").innerHTML = exp;
+	
+	if (en <= 0) {
+		document.getElementById("night").innerHTML = change_night('+', 1);
+		document.getElementById("blood").innerHTML = change_blood('-', 1);
+		document.getElementById("energy").innerHTML = change_energy('+', Math.abs(en) + 1);
+		
+		if (bld <= 0) { 
+			window.alert("You have failed to acquire enough blood to survive the day.\n" + 
+				"Your corpse does not rise at dusk.\n" + 
+				"The End.");
+		}
+	}
+}
