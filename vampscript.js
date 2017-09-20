19aca16161e78ca755b09f8a48a5787d0ac13e18
Samuel A. Lamb <samuei@users.noreply.github.com>
2017-09-20 01:19:15 -0500

    Equivalence
    You hunt differently, but it makes no tangible difference.

--- vampscript.js
+++ vampscript.js
@@ -1,3 +1,26 @@
+window.onload = function() {
+	document.getElementById("hunt").addEventListener('click', function() {
+
+		var bld = change_blood('+', 1),
+			en = change_energy('-', 1),
+			exp = change_experience('+', 1);
+		document.getElementById("blood").innerHTML = bld;
+		document.getElementById("energy").innerHTML = en;
+		document.getElementById("experience").innerHTML = exp;
+		
+		if (en <= 0) {
+			document.getElementById("night").innerHTML = change_night('+', 1);
+			document.getElementById("blood").innerHTML = change_blood('-', 1);
+			document.getElementById("energy").innerHTML = change_energy('+', Math.abs(en) + 1);
+			
+			if (bld <= 0) { 
+				window.alert("You have failed to acquire enough blood to survive the day.\n" + 
+					"Your corpse does not rise at dusk.\n" + 
+					"The End.");
+			}
+		}
+	}, false);
+};
 
 var change_blood = (function () {
     var counter = 1;
@@ -30,24 +53,3 @@ var change_night = (function () {
 		else if (op == '-') return counter -= amt;
 	}
 })();
-
-function drink(){
-	var bld = change_blood('+', 1),
-		en = change_energy('-', 1),
-		exp = change_experience('+', 1);
-    document.getElementById("blood").innerHTML = bld;
-    document.getElementById("energy").innerHTML = en;
-    document.getElementById("experience").innerHTML = exp;
-	
-	if (en <= 0) {
-		document.getElementById("night").innerHTML = change_night('+', 1);
-		document.getElementById("blood").innerHTML = change_blood('-', 1);
-		document.getElementById("energy").innerHTML = change_energy('+', Math.abs(en) + 1);
-		
-		if (bld <= 0) { 
-			window.alert("You have failed to acquire enough blood to survive the day.\n" + 
-				"Your corpse does not rise at dusk.\n" + 
-				"The End.");
-		}
-	}
-}
