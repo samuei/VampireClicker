4dad85131a077dd05269c262bfaaa76d88edc584
Samuel A. Lamb <samuei@users.noreply.github.com>
2017-09-20 23:09:55 -0500

    Objectification
    You learn to look back at nights gone by, at the people who were food.
    
    This history does not yet bring new skills.

--- vampscript.js
+++ vampscript.js
@@ -1,55 +1,56 @@
 window.onload = function() {
-	document.getElementById("hunt").addEventListener('click', function() {
-
-		var bld = change_blood('+', 1),
-			en = change_energy('-', 1),
-			exp = change_experience('+', 1);
-		document.getElementById("blood").innerHTML = bld;
-		document.getElementById("energy").innerHTML = en;
-		document.getElementById("experience").innerHTML = exp;
-		
-		if (en <= 0) {
-			document.getElementById("night").innerHTML = change_night('+', 1);
-			document.getElementById("blood").innerHTML = change_blood('-', 1);
-			document.getElementById("energy").innerHTML = change_energy('+', Math.abs(en) + 1);
-			
-			if (bld <= 0) { 
-				window.alert("You have failed to acquire enough blood to survive the day.\n" + 
-					"Your corpse does not rise at dusk.\n" + 
-					"The End.");
-			}
-		}
-	}, false);
+	// Listeners
+	document.getElementById("hunt").addEventListener('click', function() {vamp_object.hunt()}, false);
 };
 
-var change_blood = (function () {
-    var counter = 1;
-    return function (op, amt) {
-		if (op == '+') return counter += amt;
-		else if (op == '-') return counter -= amt;
+// Primary game object
+var vamp_object = {
+	blood : 1,
+	energy : 1,
+	experience : 0,
+	night : 1,
+	buffer : "You are a blood-sucking creature of the night.<br>" +
+		"<br>You require the blood of the living to animate your soulless corpse every sunset." + 
+		"<br>Perhaps as you grow and learn about the night, you will become able to do more than simply feed and sleep. " + 
+		"<br>Perhaps. Or perhaps not. ",
+	
+	hunt : function() {
+		this.blood += 1;
+		this.energy -= 1;
+		this.experience += 1;
+		document.getElementById("blood").innerHTML = this.blood;
+		document.getElementById("energy").innerHTML = this.energy;
+		document.getElementById("experience").innerHTML = this.experience;
+		
+		
+		if (this.energy <= 0) {
+			this.night += 1;
+			this.blood -= 1;
+			this.energy += Math.abs(this.energy) + 1;
+			document.getElementById("night").innerHTML = this.night;
+			document.getElementById("blood").innerHTML = this.blood;
+			document.getElementById("energy").innerHTML = this.energy;
+			debugger;
+			if (this.blood <= 0) { 
+				this.message("<br>You have failed to acquire enough blood to survive the day.<br>" + 
+					"Your corpse does not rise at dusk.<br><br>" + 
+					"The End.<br>");
+				document.getElementById("hunt").style.display = 'none';
+				return;
+			}
+			
+			this.message("You corner a hobo and take your fill.");
+		}
+	},
+	
+	message : function(in_text) {
+		// Join the buffer with the new text
+		var split_buffer = this.buffer.split('<br>').concat(in_text.split('<br>'));
+		// Only want the last 8 lines
+		if (split_buffer.length > 8) {
+			split_buffer = split_buffer.slice(-8);
+		}
+		this.buffer = split_buffer.join('<br>');
+		document.getElementById("center_panel").innerHTML = this.buffer;
 	}
-})();
-
-var change_energy = (function () {
-    var counter = 1;
-    return function (op, amt) {
-		if (op == '+') return counter += amt;
-		else if (op == '-') return counter -= amt;
-	}
-})();
-
-var change_experience = (function () {
-    var counter = 0;
-    return function (op, amt) {
-		if (op == '+') return counter += amt;
-		else if (op == '-') return counter -= amt;
-	}
-})();
-
-var change_night = (function () {
-    var counter = 0;
-    return function (op, amt) {
-		if (op == '+') return counter += amt;
-		else if (op == '-') return counter -= amt;
-	}
-})();
+};
